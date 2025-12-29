f01e881c093ad04ffc2f709ccb3180f766f7c1fc
Hamidur Rahman Majed <2022334042@student.sust.edu>
2025-12-30 00:49:05 +0600

    Refactor Stack class methods and remove duplicates

--- stack.py
+++ stack.py
@@ -1,26 +1,23 @@
 class Stack:
 
-def __init__(self,items):
-self.items = items.copy
+  def __init__(self,items):
+    self.items = items.copy
+  
+  def push(self,item):
+    self.items.append(item)
+  
+  def pull(self):
+    return self.items.pop(-1)
 
+  def show(self):
+    return self.items
 
-def push(self,item):
-self.items.append(item)
-
-
-def pull(self):
-return self.items.pop(-1)
-
-
-def show(self):
-return self.items
-
-def peek(self):
-return self.items[-1]
+  def peek(self):
+    return self.items[-1]
 s1 = Stack([])
 s2 = Stack([])
 for i in range(4):
-s2.push(i)
+  s2.push(i)
 s2.push('new')
 s2.pull()
 print(s2.peek())
